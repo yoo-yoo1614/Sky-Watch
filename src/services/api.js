8e7cbf5f6ec56016f7a316bad80e76a336a37c86
yoo-yoo1614 <harshit.dms@gmail.com>
2023-09-20 15:40:46 +0530

    api updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = 'bd79192c2d8dd1be421c2444d93a0d42';
-const API_URL = 'http://api.openweathermap.org/data/2.5/weather';
+const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
 export const getWeather = async (city, country) => {
     try {
